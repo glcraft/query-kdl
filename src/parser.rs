849747d4499d7a903c96af6c6a153b1c2b9122e3
Gabin Lefranc <gabin.lefranc@gmail.com>
2025-03-30 03:02:00 +0200

    fix path display

--- src/parser.rs
+++ src/parser.rs
@@ -97,7 +97,6 @@ pub struct Path<'a> {
 
 impl<'a> Display for Path<'a> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "/")?;
         for node in &self.nodes {
             write!(f, "{node}/")?;
         }
